dd6c760f8f037d3361d817ed1a7120f3fd796b37
Emile RICAUD <emile.ricaud@gmail.com>
2024-01-26 22:47:01 +0100

    first commit

---
+++ aft-api/src/main/java/com/ericaud/AftApiApplication.java
@@ -0,0 +1,13 @@
+package com.ericaud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AftApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AftApiApplication.class, args);
+    }
+
+}
---
+++ aft-web/src/main/java/com/ericaud/AftWebApplication.java
@@ -0,0 +1,13 @@
+package com.ericaud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AftWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AftWebApplication.class, args);
+    }
+
+}
